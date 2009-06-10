ff234da5d6a11f02370e438fd5b4e764b8a7c1f8
guillem <guillem@mestral.(none)>
2009-06-10 18:02:28 +0200

    afegida side right

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <html>
 
     <head>
-        <title>Tema es liceu</title>
+        <title>Es Liceu</title>
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 
     </head>
     <body>
         <div id="page">
-            <div id="header">CABECERAaaaaaaaaaaaaaaaa</div>
+            <div id="header">Es Liceu</div>
     
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
-        
+<?php get_sidebar("right"); ?>        
 
             <div id="content">CONTENIDO</div>
             
