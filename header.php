2a94c6e9938b026ec0b98e86cddf108deef7c7da
pnegre <pnegre@andromeda.lan>
2009-09-15 00:26:19 +0200

    Anem fent...

--- header.php
+++ header.php
@@ -1,21 +1,50 @@
 <html>
 
-    <head>
-        <title>Es Liceu</title>
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+<head>
+	<title>Es Liceu</title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+	
+	<link rel="stylesheet" type="text/css" href="/wp-content/themes/liceuWP/jquerycssmenu.css" />
+	<script type="text/javascript" src="/wp-content/themes/liceuWP/jquery.js"></script>
+	<script type="text/javascript" src="/wp-content/themes/liceuWP/jquerycssmenu.js"></script>
+	
 
-    </head>
-    <body>
-        <div id="header">
-		<div class="esquerra">
-			<a href=""<?php echo get_option('home'); ?>/">&nbsp;</a>
-		</div>
-		<div class="dreta">
-			<ul>
-				<li><a href="http://wiki.esliceu.com">Wiki</a></li>
-				<li>|</li>
-				<li><a href="/">Pàgina principal </a></li>
-			</ul>
-		</div>
+
+</head>
+<body>
+	<div id="header">
+	<div class="esquerra">
+		<a href=""<?php echo get_option('home'); ?>/">&nbsp;</a>
 	</div>
+	<div class="dreta">
+		<ul>
+			<li><a href="http://wiki.esliceu.com">Wiki</a></li>
+			<li>|</li>
+			<li><a href="/">Pàgina principal </a></li>
+		</ul>
+	</div>
+
+<div id="myjquerymenu" class="jquerycssmenu"> 
+<ul>
+ 		<li> <a href="#">  L'escola  </a>
+			<ul>
+				<li> <a href="#">  Trets d'identitat  </a>   </li>
+				<li> <a href="#">  Per trobar-nos  </a>   </li>
+				<li> <a href="#">  APIMA  </a>   </li>
+				<li> <a href="#">  Notícies generals  </a>   </li>
+			</ul>
+		</li>
+ 		
+		<li> <a href="#">  Projectes  </a>
+			<ul>
+				<li> <a href="#">  sub21  </a>   </li>
+				<li> <a href="#">  sub22  </a>   </li>
+			</ul>
+ 		</li>
+</ul>
+</div>
+
+
+
+</div>
     
