1a59a9fb6528b9aa1de95a6d8054ff07638aade4
pnegre <pnegre@yorkie.(none)>
2010-02-25 12:58:50 +0100

    Anem fent...

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
-            <div id="footer">
+
+<div id="footer">
 	    <div>
 		Es Liceu. Carrer Cabana, 31. 07141, Pont d'Inca, Marratxí
 		<br/>
@@ -9,9 +10,8 @@
 		Telèfon: 971 60 09 86
 	    </div>
 	    
-	    </div>
-    </body>
-    
-    
+</div>
+
+</body>
 
 </html>
--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <html>
 
 <head>
+
 	<title>Es Liceu</title>
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 	
@@ -21,24 +22,8 @@
 <div id="containermenu">
 	<div id="myjquerymenu" class="jquerycssmenu"> 
 	<ul>
-			<!--<li> <a href="#">  L'escola  </a>
-				<ul>
-					<li> <a href="#">  Trets d'identitat  </a>   </li>
-					<li> <a href="#">  Per trobar-nos  </a>   </li>
-					<li> <a href="#">  APIMA  </a>   </li>
-					<li> <a href="#">  Notícies generals  </a>   </li>
-				</ul>
-			</li>
-			
-			<li> <a href="#">  Projectes  </a>
-				<ul>
-					<li> <a href="#">  sub21  </a>   </li>
-					<li> <a href="#">  sub22  </a>   </li>
-				</ul>
-			</li>-->
 
 		 <?php wp_list_pages('title_li=') ?>
-
 
 	</ul>
 	</div>
