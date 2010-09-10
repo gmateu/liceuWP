0b5a0138ef0fceb97af83e97e01fff8a08993156
Pere <pere.negre@gmail.com>
2010-09-10 12:41:21 +0200

    Integrada nova plantilla. Comencem un altre cop

--- index.php
+++ index.php
@@ -1,50 +1,203 @@
-<?php get_header(); ?>  
+<html>
 
-<table id="table_sidebar">
+
+<head>
+
+<base href="/wordpress/wp-content/themes/liceuWP/">
+
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link rel="StyleSheet" href="style.css" type="text/css"> 
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<script type="text/javascript" language="JavaScript" src="scripts.js"></script>
+
+
+
+</head>
+
+
+<body>
+
+<div id="main_container">
+
+
+<div id="head1">
+<img src="imgs/logo_esliceu.png">
+<p>
+
+<input type="text" value="Cerca">
+<!--<input type="button">-->
+
+Localització | Contacte
+</p>
+</div>
+
+<div id="menu1">
+	<span class="selected"> L'escola </span>
+	<span> Estudis </span>
+	<span> Serveis </span>
+	<span> Notícies </span>
+</div>
+
+<div id="menu2">
+	<span> Presentació </span>
+	<span> Trets d'identitat </span>
+	<span> Història </span>
+	<span> L'edifici </span>
+	<span> Òrgan directiu </span>
+	<span> Contacte </span>
+</div>
+
+<div id="fade1">
+</div>
+
+<table id="maintable">
+
 <tr>
-<td>
+<td id="first_column">
 
-		<div id="content">
-		<?php if (have_posts()) : ?>
-		<?php while (have_posts()) : the_post(); ?>
+<div id="tit_recorda">Recorda!</div>
+<div id="recorda">Els horaris d'examens de setembre els podeu llegir aquí. Així mateix podeu veure la llista de llibres pel curs vinent a "Administració i serveis".</div>
 
-			<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
-				<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-				<small><?php the_time('F jS, Y') ?> <!-- by <?php the_author() ?> --></small>
+<div id="tit_noticies" class="titbar">Notícies i actualitat</div>
+<div id="not_destacada">
+	<img src="imgs/imatge_exemple_allots.png">
+	<p>
+	Catàleg d'activitats estiuenques: Grup de biblioteques infantils
+	</p>
+	<div>
+	<span> <b>Primària</b> | 30/8/2010 </span>
+	Recurs que us pot ser d'utilitat si treballeu amb públic infantil. El grup de treball de Biblioteques Infantils i Juvenils ha preparat un document sobre
+	el tema de les activitats de lectura
+	</div>
+</div>
 
-				<div class="entry">
-					<?php the_content('Read the rest of this entry &raquo;'); ?>
-				</div>
+<div id="altres_destacades">
 
-				<p class="postmetadata"><?php the_tags('Tags: ', ', ', '<br />'); ?> Categories: <?php the_category(', ') ?> </p>
-			</div>
-			<div id="clearer"></div>
-			<div class="postseparator"></div>
+	<div class="destacada2">
+		<table><tr>
+			<td>
+			<div class="cat_i_data"> <b>ESO i PQPI</b> 30/8/2010 </div>
+			<div class="tit"> Es Liceu participa al programa de Seccions Europees </div>
+			</td>
+			<td>
+			<img src="imgs/im1.png">
+			</td>
+		</tr></table>
+		<div class="text"> Escrit dimecres a la nit: Tancam el curs i les activitats Comenius amb la darrera trobada d'aquest curs acadèmic a Klaipeda, Lituània. Partim!!!</div>
+	</div>
+	
+	<div class="separator"></div>
+	
+	<div class="destacada2">
+		<table><tr>
+			<td><div class="cat_i_data"> <b>Infantil</b> 30/8/2010 </div>
+			<div class="tit"> Els polls: informació per pares, mares i mestres </div></td>
+			<td><img src="imgs/im2.png"></td>
+		</tr></table>
+		<div class="text"> De manera preventiva, abans que arribi la primavera, us enviem aquesta informació sobre com prevenir
+		i tractar la presència d'aquestes petites criatures, habitants dels cabells.</div>
+	</div>
 
-		<?php endwhile; ?>
-		<?php else : ?>
-		    <h2 class="center">Not Found</h2>
-		    <p class="center">Sorry, but you are looking for something that isn't here.</p>
-		    <?php get_search_form(); ?>
-		<?php endif; ?>
-	    
-	    
-	    </div>
+</div>
+
+<div id="altres_noticies">
+
+
+<div class="not_ordinaria">
+	<img src="imgs/im3.png">
+	<div class="fright">
+		<div class="cat_i_data"> <b>Formació professional</b> 30/8/2010 </div>
+		<div class="tit"> Final de curs tercer cicle </div>
+		<div class="text"> Els alumnes del cicle formatiu en Informàtica han arribat de Londres. Pitjant aquí en veureu el reportatge.No els ha agradat gens Picadilly.</div>
+	</div>
+</div>
+
+<div class="separator"></div>
+
+<div class="not_ordinaria">
+	<img src="imgs/im4.png">
+	<div class="fright">
+		<div class="cat_i_data"> <b>Primària</b> 30/8/2010 </div>
+		<div class="tit"> FESTA FINAL INFANTIL </div>
+		<div class="text"> Aquí teniu una mostra de la petita festa que varen fer els infants d'educació infantil el darrer dia d'escola. Aprofitam per a acomiadar-nos de na Cati i de na Lluc. Molta sort i moltes gràcies per tot!!! I a tots i a totes BON ESTIU!!!!</div>
+	</div>
+</div>
+
+<div class="separator"></div>
+
+<div class="not_ordinaria">
+	<img src="imgs/im5.png">
+	<div class="fright">
+		<div class="cat_i_data"> <b>Primària</b> 30/8/2010 </div>
+		<div class="tit"> Primer concurs de medi ambient d'Es Liceu </div>
+		<div class="text"> Els nins i les nines de les classes de 4 anys A d'Educació Infantil i 3r A d'Educació Primària han guanyat. Aquí vos deixam un petita mostra amb algunes imatges dels guanyadors i de les guanyadores.</div>
+	</div>
+</div>
+
+
+
+<div id="mes_actualitat"> Més actualitat </div>
+
+
+
+
+</div>
+
 </td>
-<td>
 
-	<ul id="nav_right">
-	<?php if ( !function_exists('dynamic_sidebar')
-			|| !dynamic_sidebar() ) : ?>
+<td id="second_column">
 
-
-	<?php endif; ?>
-	</ul>
-
+	<div id="container_menu">
+		<div class="titbar bar">Destaquem</div>
+		<p class="link_leftmenu"> Activitats d'estiu </p>
+		<p class="link_leftmenu"> Preguntes Freqüents </p>
+		<p class="link_leftmenu"> Divertits </p>
+		<p class="link_leftmenu"> Àlbum fotogràfic </p>
+		<p class="link_leftmenu">
+			<img src="imgs/portes_obertes.png">
+		</p>
+		<p class="link_leftmenu">
+			<img src="imgs/poema_mes.png">
+		</p>
+		
+		
+		<div class="titbar bar">Alumnat</div>
+		<p class="link_leftmenu"> Calendari escolar </p>
+		<p class="link_leftmenu"> Matrícula </p>
+		<p class="link_leftmenu"> Wiki </p>
+		
+		
+		<div class="titbar bar">Personal docent</div>
+		<p class="link_leftmenu"> Correu intern </p>
+		<p class="link_leftmenu"> Incidències </p>
+		<p class="link_leftmenu"> Wiki </p>
+		
+	
+	</div>
 
 </td>
 </tr>
-</table>
-            
-<?php get_footer(); ?>
 
+</table>
+
+
+
+
+
+</div> <!-- end of div#main_container -->
+
+<div id="footer">
+
+<b> Es Liceu: </b> <i>Carrer Cabana, 31. 07171 Pont d'Inca, Marratxí.</i> <br>
+<b> Telèfon: </b> 971 60 09 86 <b> &nbsp; &nbsp;  Fax: </b> 971 79 48 09 <b> &nbsp; &nbsp;   Correu: </b> escola@esliceu.com
+
+</div>
+
+
+
+</body>
+
+
+
+</html>
